f29dc87672364f3101260e6d57fbf69ebe0c28af
Juan E Gonzalez <158267755+juan007-play@users.noreply.github.com>
2024-08-03 06:09:12 +0000

    juane_domain_name_generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net", ".us", ".io"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let dot = 0; dot < domain.length; dot++) {
+        let combination = `${pronoun[i]} ${adj[a]} ${noun[n]}${domain[dot]}`;
+        console.log(combination);
+      }
+    }
+  }
+}
